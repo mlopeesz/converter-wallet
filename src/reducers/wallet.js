a1f004f0235762f8cc859941a00524e0d28b3d5e
Mauricio Lopes <mlopeesz@gmail.com>
2022-01-12 15:36:54 -0300

    Adicionado reducer para Wallet

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,24 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { WALLET_USER, WALLET_FETCH } from '../actions';
+
+const INITIAL_STATE = {
+  expenses: [],
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case WALLET_USER:
+    return {
+      ...state,
+      expenses: [...state.expenses, action.payload],
+    };
+  case WALLET_FETCH:
+    return {
+      ...state,
+    };
+  default:
+    return state;
+  }
+};
+
+export default wallet;
