0bafa13b50c38faadaa7814e4a5f4f5d64c9dfca
Mauricio Lopes <mlopeesz@gmail.com>
2022-01-12 15:29:14 -0300

    Alteraçao nas actions com as API's

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,17 @@
-export const LOGIN_EMAIL = 'LOGIN_EMAIL';
+import { apiCurrency } from '../services/apiCurrency';
 
+export const LOGIN_EMAIL = 'LOGIN_EMAIL';
 export const loginEmail = (email) => ({
   type: LOGIN_EMAIL,
   email,
 });
+
+export const WALLET_FETCH = 'WALLET_FETCH';
+export const walletFetch = (payload) => ({
+  type: WALLET_FETCH,
+  payload,
+});
+
+export const getCurrency = (currency) => async (dispatch) => {
+  await apiCurrency(currency).then((response) => dispatch(walletFetch(response)));
+};
