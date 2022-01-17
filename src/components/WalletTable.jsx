93c2e9f030fa696a3dde66d990904927de06db50
Mauricio Lopes <mlopeesz@gmail.com>
2022-01-17 10:00:53 -0300

    Renderiza informações na tabela

--- src/components/WalletTable.jsx
+++ src/components/WalletTable.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class WalletTable extends React.Component {
   render() {
+    const { expenses } = this.props;
     return (
-      <table className="table">
+      <table>
         <thead>
           <tr>
             <th>Descrição</th>
@@ -17,9 +20,45 @@ class WalletTable extends React.Component {
             <th>Editar/Excluir</th>
           </tr>
         </thead>
+        <tbody className="tbody">
+          {expenses.map(({
+            id,
+            value,
+            tag,
+            method,
+            description,
+            currency,
+            exchangeRates,
+          }) => (
+            <tr key={ id } className="tr">
+              <td className="td">{description}</td>
+              <td className="td">{tag}</td>
+              <td className="td">{method}</td>
+              <td className="td">{value}</td>
+              <td className="td">{currency}</td>
+              <td className="td">{exchangeRates[currency].name.split('/')[0]}</td>
+              <td className="td">
+                {
+                  (Number(exchangeRates[currency].ask)
+                     * Number(value)).toFixed(2)
+                }
+              </td>
+              <td className="td">{ Number(exchangeRates[currency].ask).toFixed(2) }</td>
+              <td className="td">Real</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     );
   }
 }
 
-export default WalletTable;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+WalletTable.propTypes = {
+  expenses: PropTypes.arrayOf(Object).isRequired,
+};
+
+export default connect(mapStateToProps)(WalletTable);
